ea453316b06c9376ac0bfbe8b7c869cdc6ccce89
CTC3PO <chuinus@gmail.com>
2023-06-08 23:38:48 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,121 @@
-import logo from './logo.svg';
-import './App.css';
+// REACT THREE FIBER WAY
+
+import { Suspense, useRef } from "react";
+// import { useRef } from "react";
+import { Canvas, useLoader, useFrame } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import {
+  Environment,
+  Html,
+  OrbitControls,
+  useProgress,
+} from "@react-three/drei";
+
+function Loader() {
+  const { progress } = useProgress();
+  return <Html center>{progress} % loaded</Html>;
+}
+
+const FreeFarm = (args) => {
+  const farmRef = useRef();
+  useFrame(() => {
+    if (farmRef.current) {
+      farmRef.current.rotation.x += 0.05;
+      farmRef.current.rotation.y += 0.1;
+      farmRef.current.position.z = (farmRef.currentposition.z + 0.01) % 10;
+    }
+  });
+  const gltf = useLoader(GLTFLoader, "./london-model/scene.gltf");
+  return <primitive object={gltf.scene} {...args} />;
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas style={{ height: "100vh" }}>
+      <Suspense fallback={<Loader />}>
+        <pointLight color="orange" position={[10, 10, 10]} />
+        <FreeFarm position={[0, 0, 0]} scale={0.05} rotation-x={1} />
+        <OrbitControls
+          enableZoom={true}
+          rotateSpeed={2}
+          autoRotate={true}
+          autoRotateSpeed={1}
+        />
+        <Environment preset="park" background />
+      </Suspense>
+    </Canvas>
   );
 }
 
 export default App;
+
+// import { useEffect, useRef } from "react";
+// import { Suspense } from "react";
+// import { canvas, useLoader } from "@react-three/fiber";
+// import * as THREE from "three";
+
+// function App() {
+//   const divRef = useRef();
+
+//   useEffect(() => {
+//     // init
+//     let width = divRef.current.clientWidth;
+//     let height = divRef.current.clientHeight;
+
+//     const camera = new THREE.PerspectiveCamera(
+//       70,
+//       window.innerWidth / window.innerHeight,
+//       0.01,
+//       10
+//     );
+//     camera.position.z = 2;
+
+//     const scene = new THREE.Scene();
+
+//     const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
+//     const material = new THREE.MeshNormalMaterial();
+
+//     const mesh = new THREE.Mesh(geometry, material);
+//     scene.add(mesh);
+
+//     const renderer = new THREE.WebGLRenderer({ antialias: true });
+//     renderer.setSize(window.innerWidth, window.innerHeight);
+//     renderer.setAnimationLoop(animation);
+
+//     const divCurrent = divRef.current;
+//     divCurrent.appendChild(renderer.domElement);
+
+//     window.addEventListener("resize", handleResize);
+
+//     //handle window resize
+//     function handleResize() {
+//       width = divRef.current.clientWidth;
+//       height = divRef.current.clientHeight;
+//       renderer.setSize(width, height);
+//       camera.aspect = width / height;
+//       camera.updateProjectionMatrix();
+//       renderer.render(scene, camera);
+//     }
+
+//     // animation
+//     function animation(time) {
+//       mesh.rotation.x = time / 2000;
+//       mesh.rotation.y = time / 1000;
+
+//       renderer.render(scene, camera);
+//     }
+
+//     return () => {
+//       renderer.setAnimationLoop(null);
+//       window.removeEventListener("resize", handleResize);
+//       divCurrent.removeChild(renderer.domElement);
+//       scene.remove(mesh);
+//       geometry.dispose();
+//       material.dispose();
+//     };
+//   }, []);
+
+//   return <div ref={divRef} style={{ height: "100vh" }} />;
+// }
+
+// export default App;
